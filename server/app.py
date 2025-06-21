5e9b1cf4f58d346631e6e41aed51a93370290c40
Bildad Ereggae <bildadereggae@gmail.com>
2025-06-21 15:18:24 +0300

    GET api lab done

--- server/app.py
+++ server/app.py
@@ -15,13 +15,56 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return "Index for Game/Review/User API"
 
+
 # start building your API here
+@app.route("/games")
+def games():
+    # games = []
+    # for game in Game.query.all():
+    #     game_dict = game.to_dict()
+    #     games.append(game_dict)
+
+    # response = make_response(games, 200, {"Content-Type": "application/json"})
+    games = [game.to_dict() for game in Game.query.all()]
+
+    response = make_response(games, 200)
+
+    return response
 
 
-if __name__ == '__main__':
+@app.route("/games/<int:id>")
+def game_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+
+    game_dict = game.to_dict()
+
+    response = make_response(game_dict, 200)
+
+    return response
+
+
+@app.route("/games/users/<int:id>")
+def game_users_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+    # users = []
+    # for review in game.reviews:
+    #     user = review.user
+    #     user_dict = user.to_dict(rules=("-reviews",))
+    #     users.append(user_dict)
+
+    # response = make_response(users, 200)
+    #using comprehension to make the code succint
+    # users = [review.user.to_dict(rules=("-reviews",)) for review in game.reviews]
+    # response = make_response(users, 200)
+    users = [user.to_dict(rules=("-reviews",)) for user in game.users]
+    response = make_response(users, 200)
+    return response
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
-
